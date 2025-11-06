2c1f1e6bfa676beb7d1d8083e6f8e341ff5ae710
itsdxd <darryl.day@gmail.com>
2025-11-06 11:34:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ terraform {
 }
 
 resource "google_compute_network" "example_vpc" {
-  name                    = "example-vpc"
+  name                    = "example-dd-vpc"
   auto_create_subnetworks = true 
 }
